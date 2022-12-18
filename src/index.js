8edf321e8aa31c4bd6cebdc8e70aeec11f925b84
kamome-829 <tokunaga8695@gmail.com>
2022-12-18 18:55:57 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { MoralisProvider } from "react-moralis";
+import { TransactionProvider } from './context/TransactionContext';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
+  <TransactionProvider>
+    <React.StrictMode>
+    <MoralisProvider>
+      <App />
+    </MoralisProvider>
   </React.StrictMode>
+  </TransactionProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
